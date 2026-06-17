e1ab49c52c836178cd481d3023176dc62f4549b4
Ananya Gupta <imguptaananya@gmail.com>
2026-06-17 18:36:58 +0530

    Add Weird Algorithm implementation in C++

---
+++ CSES/Introductory_Problems/Weird_Algorithm.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    while(x!=1){
+        cout<<x<<" ";
+        if(x%2==0){
+            x=x/2;
+        }
+        else{
+            x= 3*x +1;
+        }
+    }
+    cout<<1;
+}
