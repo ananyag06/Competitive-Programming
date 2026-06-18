28428fbe856ca283bca3649e3ecd71f99a4fdea8
Ananya Gupta <imguptaananya@gmail.com>
2026-06-18 11:46:35 +0530

    Update MissingNumber.cpp

--- CSES/Introductory_Problems/MissingNumber.cpp
+++ CSES/Introductory_Problems/MissingNumber.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main()
@@ -6,16 +6,18 @@ int main()
     int n; 
     cin >> n;
 
-    long long sum = 1LL * n * (n + 1) / 2; // instead we 
-
+    long long total = 1LL * n * (n + 1) / 2; 
+    // 1LL forces the expression to be evaluated as long long.
+    // Otherwise n*(n+1) would be computed as int*int first, which will lead to overflow.
+    long long sum = 0;
     for(int i = 0; i < n - 1; i++)
     {
         int x;
         cin >> x;
-        sum -= x;
+        sum += x; // sum = sum +x
     }
 
-    cout << sum;
+    cout << total -sum; 
 
     return 0;
 }
