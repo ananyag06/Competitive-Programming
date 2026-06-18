c513ea0e6d772b6aceeb2af83e6ccf85a8d94e47
Ananya Gupta <imguptaananya@gmail.com>
2026-06-18 18:12:08 +0530

    Update IncreasingArray.cpp

--- CSES/Introductory_Problems/IncreasingArray.cpp
+++ CSES/Introductory_Problems/IncreasingArray.cpp
@@ -1,1 +1,19 @@
-
+#include <bits/stdc++.h> //Includes a lot of libraries like iostream, vector, map so on within it
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector <long long> a(n);
+    long long sum = 0;
+    for(int i =0; i<n; i++){
+        cin>>a[i];
+    }
+    for(int i =1; i<n; i++){
+        if(a[i-1]>a[i]){
+            sum += a[i-1]- a[i];
+            a[i]= a[i-1];
+        }
+    }
+    cout<<sum;
+    return 0;
+}
