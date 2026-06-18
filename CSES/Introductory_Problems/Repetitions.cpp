71904dd5209e7b670076507ec7654a5ec20b4a54
Ananya Gupta <imguptaananya@gmail.com>
2026-06-18 11:48:28 +0530

    Update Repetitions.cpp

--- CSES/Introductory_Problems/Repetitions.cpp
+++ CSES/Introductory_Problems/Repetitions.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int current =1;
+    int best =1;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.length(); i++){
+        if(s[i]==s[i+1]){
+            current++;
+        }
+        else{
+            current=1;
+        }
+        if(current>best){ //best = max(best, current); I can directly use this also
+            best=current;
+        }
+    }
+    cout<<best;
+    return 0;
+}
